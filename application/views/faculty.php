886d8fea6e9a9f09aca3be37b02bd6527bf0bb34
Maynk <m.charly3015@gmail.com>
2019-11-26 07:00:42 +0530

    fsculty page design

--- application/views/faculty.php
+++ application/views/faculty.php
@@ -1,12 +1,70 @@
-<!-- Side navigation -->
-<div class="sidenav">
-  <a href="#">About</a>
-  <a href="#">Services</a>
-  <a href="#">Clients</a>
-  <a href="#">Contact</a>
-</div>
 
-Hello there 
-<div class="main">
-  ...
-</div>
+
+
+<!-- -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-  Main section =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- -->
+	    
+		
+<!-- Left Sidebar -->
+
+<section class="container-fluid">
+	<div class="row">
+	    <div class="col-md-12" style="background-color:RGB(245,245,245);">
+	        <div class="row">
+	        	<div class="col-md-2" id="left-sidebar"  style="margin-top:1%">
+	        		
+	        		<!-- Accordian left-sidebar  starts -->
+	            	<div class="list-group">
+	            		<a href="#" class="list-group-item active"><b>Archives</b></a>
+					  	  </div>
+	              	<!-- Accordian ends -->
+	         </div>
+
+				
+
+
+				<!-- Main carousels Section -->
+
+				<div class="col-md-9" id="main-section" style="margin-top:1%;padding:5%">
+					<div class="row">
+						<div class="col-md-12">
+
+							<div class="topslider">
+		            
+		        			
+        					<div class="col-md-11 text-justify" style="margin-left:3%;padding-bottom:2%;">
+        						<div class="separator"></div>
+								<h4><b>Welcome to IJBAAR:</b></h4>
+								<div class="separator"></div>
+								<span>International Journal of Basic and Applied Agricultural Research (ISSN: 0972-8813)
+								 is a brisk multidisciplinary research publication platform providing those individuals an 
+								 ideal opportunity to accomplish their desires who love to innovate and share their ideas
+								  to the people in the field.  Designed to usher the sublimity around the globe we engage 
+								  our noble efforts for the enlightenment of multidisciplinary issues contemplatively. We
+								   bring forth this agricultural journal triennially (three issues in a year) and promote 
+								   the vivid research entries with precise and apt touches and by bridging the gulf between 
+								   perception and the inception. IJBAAR publishes only original research work related to 
+								   Agriculture and allied fields and follow stringent double blind review process. The review
+								    process is supported by a dedicated team of national and International experts. ... 
+								    
+								</span>
+        					</div>
+		    		
+					</div>
+						
+		        </div>
+
+	  			
+
+	  			
+
+
+
+	  		
+	              	</div>
+	            </div>
+	        </div>
+
+	       
+	    </div>
+	</div>
+</section>

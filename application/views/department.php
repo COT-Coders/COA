1e8be1a1f8dbfdde99ef681dc507831edc1c53d8
Maynk <m.charly3015@gmail.com>
2019-12-09 05:01:08 +0530

    Department home page design start

--- application/views/department.php
+++ application/views/department.php
@@ -1,7 +1,7 @@
 <section class="container-fluid">
 	<div class="row">
         <div class="col-md-1"></div>
-	    <div class="col-md-10" style="background-color:RGB(245,245,245);">
+	    <div class="col-md-8" style="background-color:RGB(245,245,245);">
 
             <h2> About Agrometeorology </h2>
             <div class="row">
@@ -83,5 +83,22 @@
 
             
         </div>
+
+         <!--- Right Side Bar for departent specific Information -->
+
+        <div class="col-md-2" style="background-color:RGB(245,245,245);">
+        <div class="panel panel-primary">
+            <div class="panel-heading">News</div>
+            <div class="panel-body">
+                <ul>
+                    <li>content 1 </li>
+                    <li> content 2</li>
+                    <li> content 3 </li>
+                    <li> content 4 </li>
+                    <li> content 5 </li>
+                </ul>
+            </div>
+        </div>
+
     </div>
 </section>
--- application/views/facultylist.php
+++ application/views/facultylist.php
@@ -1,7 +1,7 @@
 <section class="container-fluid">
 	<div class="row">
         <div class="col-md-1"></div>
-	    <div class="col-md-10" style="background-color:RGB(245,245,245);">
+	    <div class="col-md-8" style="background-color:RGB(245,245,245);">
 
 
 
@@ -73,5 +73,19 @@
 
             
         </div>
+
+        <!--- Right Side Bar for departent specific Information -->
+
+        <div class="col-md-2" style="background-color:RGB(245,245,245);">
+        <div class="panel panel-primary">
+            <div class="panel-heading">Panel with panel-primary class</div>
+            <div class="panel-body">Panel Content</div>
+        </div>
+
+        </div>
+
+
+
+
     </div>
 </section>

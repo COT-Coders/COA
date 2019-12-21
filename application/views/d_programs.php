213e66da7f3a2fc5f04fa2be0be01d48f97a8f8d
Maynk <m.charly3015@gmail.com>
2019-12-22 04:24:13 +0530

    program page

--- application/views/d_programs.php
+++ application/views/d_programs.php
@@ -3,19 +3,36 @@
         <div class="col-md-1"></div>
         <div class="col-md-8" style="background-color:RGB(245,245,245);">
         
+        <p><h4> The department of Agrometrology offers following programs </h4></p>
         <div class="panel panel-info">
-            <div class="panel-heading">Panel with panel-primary class</div>
-            <div class="panel-body">Panel Content</div>
+            <div class="panel-heading">B.Sc Agrometrology</div>
+            <div class="panel-body">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+                dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            
+            </div>
         </div>
 
         <div class="panel panel-info">
-            <div class="panel-heading">Panel with panel-primary class</div>
-            <div class="panel-body">Panel Content</div>
+            <div class="panel-heading"> M.Sc Agrometrology</div>
+            <div class="panel-body">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+                dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </div>
         </div>
 
         <div class="panel panel-info">
-            <div class="panel-heading">Panel with panel-primary class</div>
-            <div class="panel-body">Panel Content</div>
+            <div class="panel-heading">Phd Agrometrology</div>
+            <div class="panel-body">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+                dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </div>
         </div>
 
 

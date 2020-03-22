bfe0b93f4603ba19d0acfebbdd96113e9d028666
Maynk <m.charly3015@gmail.com>
2020-03-22 23:49:00 +0530

    clean the department header

--- application/views/templets/department_header.php
+++ application/views/templets/department_header.php
@@ -51,7 +51,6 @@ Developed and Designed by :: Students of College of Technology,GBPUAT pantnagar
 
 <body id="body">
 
-<<<<<<< HEAD
     <!-- -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-  Header section =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- -->
 
     <header class="container-fluid">
@@ -142,98 +141,3 @@ Developed and Designed by :: Students of College of Technology,GBPUAT pantnagar
     </header>
 
     
-=======
-	<!-- -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-  Header section =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- -->
-
-	<header class="container-fluid">
-		<div class="row" >
-			<div class="col-md-1"></div>
-			<div class="col-md-10" id="header">
-				<div class="row" style="width:98%">
-							
-					<div class="col-md-2 col-sm-2 col-xs-2 img-circle" id="logo" style="margin-left:0%;margin-top:1%;">
-							<img src="<?php echo base_url();?>assets/img/pantnagar_logo.png" alt="GBPUAT Pantnagar" width="100" height="100">
-							
-					</div>
-					
-					<div class="col-md-9 hidden-xs hidden-sm" id="title" style="margin-left:1.5%;margin-top:1.02%">
-						<div class="row">
-								<div class="col-md-6 university_link text-left">
-									<h2><a href="http://www.gbpuat.ac.in" target="_blank">College Of Agriculture</a></h2>
-									<h4 style="margin-left:1%;"><a href="http://www.gbpuat.ac.in" target="_blank">G. B. Pant Universiity of Agriculture and Technology</a></h4>
-								</div>
-						</div>
-					</div>
-					
-					<div class="col-md-9 col-xs-7 col-sm-7 hidden-md hidden-lg visible-sm-block visible-xs-block" id="title" style="margin-left:2%;">
-						<div class="row">
-								<div class="col-md-12">
-									<h3 style="font-weight:bolder; text-align:center;">College of Agriculture</h3>
-								</div>
-							
-								<div class="col-md-12 university_link" style="text-align:center;">
-									<h4><a href="http://www.gbpuat.ac.in" target="_blank">G.B.P.U.A&amp;T, Pantnagar</a></h4>
-								</div>
-						</div>
-					</div>
-					<div class="col-md-12 col-lg-12">
-						<hr>
-					</div>
-					<div class="col-md-9 hidden-xs hidden-sm" id="title" style="text-align:left;">
-						<div class="row">
-							<div class="col-md-12 university_link">
-									<h1><a href="#" target="_blank"><?php if(!empty($records)) echo $records[0]->Dept_Name; ?></a></h1>
-							</div>
-								
-						</div>
-					</div>
-				
-		  
-	  
-				</div>
-			</div>
-		</div>
-		
-
-		<!--   -=-=-=-=-=-=-=-=-=-=-=-=-=-  navigation Bar   -=-=-=-=-=-=-=-=-=-=-=-=-   -->
-		<div class="row">
-		<div class="col-md-1"></div>
-			<div class="col-md-10 navbar navbar-custom" role="navigation">
-				<div class="navbar-header">
-					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-inverse-collapse">
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-					</button>
-				</div>
-				<div class="navbar-collapse collapse navbar-inverse-collapse">
-					<ul class="nav navbar-nav navbar-main-menu" style="color:blue;font-weight:bolder;">
-					   <li><a href="<?php echo site_url(); ?>/department"><i class="fa fa-home" aria-hidden="true"></i> Home</a></li>
-					   <li><a href="<?php echo site_url(); ?>/department/program"><i class="fa fa-ellipsis-v" aria-hidden="true"></i>Programs</a></li>
-					   <li><a href="<?php echo site_url(); ?>/department/faculty"><i class="fa fa-edit" aria-hidden="true"></i> Faculty</a></li>
-					   <li><a href="<?php echo site_url(); ?>/department/staff"><i class="fa fa-reorder" aria-hidden="true"></i>Staff</a></li>
-					   <li><a href="<?php echo site_url(); ?>/department/research"><i class="fa fa-question" aria-hidden="true"></i> Research</a></li>
-					   <li><a href="#"><i class="fa fa-info-circle" aria-hidden="true"></i> Students</a></li>
-					   <li><a href="#"><i class="fa fa-paper-plane" aria-hidden="true"></i> Resources</a></li>
-					   <li><a href="#"><i class="fa fa-paper-plane" aria-hidden="true"></i> Activities</a></li>
-				   
-					</ul>
-					   
-					<ul class="nav navbar-nav navbar-right" style="font-weight:bolder;">
-						<?php
-							if($this->session->userdata('logged_in')) {
-								echo "<li><a href=".site_url()."/logout"."><span class='glyphicon glyphicon-user' aria-hidden='true'></span>"." Logout"."</a></li>";
-							}
-							else {
-								echo "<li><a href=".site_url()."/login"."><span class='glyphicon glyphicon-user' aria-hidden='true'></span>"." Login"."</a></li>";
-							}
-						?>
-						<li><a href="<?php echo site_url(); ?>/signup">SignUp</a></li>
-
-					   </ul>
-			
-				</div>
-			</div>
-		</div>
-	</header>
->>>>>>> 70c54f0a6bf5e6c4238c9e07420a41a815f6a224

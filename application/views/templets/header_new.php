4197c9bd40bce537b83dadc394b9b549f7e3037a
Maynk <m.charly3015@gmail.com>
2020-03-26 00:21:51 +0530

    new header and footer

--- application/views/templets/header_new.php
+++ application/views/templets/header_new.php
@@ -33,15 +33,19 @@
 
 		  <!-- Navbar links -->
 		  <div class="collapse navbar-collapse" id="collapsibleNavbar">
-		    <ul class="navbar-nav ml-auto">
+		    <ul class="navbar-nav ml-auto">   
 		      <li class="nav-item">
-		        <a class="nav-link" href="#">Link</a>
+		        <a class="nav-link" href="#">Home</a>
 		      </li>
 		      <li class="nav-item">
-		        <a class="nav-link" href="#">Link</a>
+		        <a class="nav-link" href="#">About Us</a>
+		      </li>
+
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">Academics</a>
 		      </li>
 		      <li class="nav-item">
-		        <a class="nav-link" href="#">Link</a>
+		        <a class="nav-link" href="#">Centers</a>
 		      </li> 
 			   <!-- Dropdown -->
 		    <li class="nav-item dropdown">

007f9bcff707f5acfc274ba5fb678721c934102d
Maynk <m.charly3015@gmail.com>
2020-03-28 20:08:57 +0530

    new header and footer for common page

--- application/views/templets/header_common.php
+++ application/views/templets/header_common.php
@@ -1,44 +1,61 @@
 <head>
-  <title>Title</title>
-  <meta charset="utf-8">
+  <!--
+  
   <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-  <link rel="stylesheet" href="<?php echo base_url();?>assets/css/custom_nav.css">
+  
   <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
   <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+-->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
   
+  <link rel="stylesheet" href="<?php echo base_url();?>assets/css/custom_nav.css">
   <link rel="stylesheet" href="<?php echo base_url();?>assets/css/footer.css">
   
 
-  
-  
-  <link rel="stylesheet" href="<?php echo base_url();?>assets/css/footer.css">
-  
-
-
+    <!------ Include the above in your HEAD tag ---------->
 </head>
-<body style="height:1500px">
-
-<!-- Grey with black text -->
-<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+<nav class="navbar navbar-expand-md navbar-dark shrink">
   <div class="container">
-  <ul class="navbar-nav">
+      
 
     <span style="padding-bottom: 10px; padding-top: 10px"><img src="<?php echo base_url();?>assets/img/pantnagar_logo.png" alt="GBPUAT Pantnagar" width="100" height="100"></span>
     <span><h3 style="color:white; padding-left:10px">College Of Agriculture</h3></span>
-    
-    <li class="nav-item active">
-      <a class="nav-link" href="#">Active</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="#">Link</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="#">Link</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link disabled" href="#">Disabled</a>
-    </li>
-  </ul>
+  
 
-</div>
+     
+
+      <!-- Navbar links -->
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+        <ul class="navbar-nav ml-auto">   
+          <li class="nav-item">
+            <a class="nav-link" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a  class="nav-link" href="<?php echo site_url(); ?>/Welcome/about">About Us</a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" href="#">Academics</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Centers</a>
+          </li> 
+         <!-- Dropdown -->
+
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#">Dropdown</a>
+          <div class="dropdown-menu">
+            <a class="dropdown-item" href="#">Link 1</a>
+            <a class="dropdown-item" href="#">Link 2</a>
+            <a class="dropdown-item" href="#">Link 3</a>
+          </div>
+        </li>
+        </ul>
+      </div>
+  </div>
 </nav>
+

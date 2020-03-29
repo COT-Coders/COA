eab4accbfa2347dffd86c59f1c9e749e3b0b2066
Maynk <m.charly3015@gmail.com>
2020-03-29 21:07:45 +0530

    home page content

--- application/views/college_home.php
+++ application/views/college_home.php
@@ -1,37 +1,54 @@
 <body>
-<div class="container">
+<div class="container mx-auto">
 <div class="row">
-<div class="card col-sm-4">
-  <img class="card-img-top" src="<?php echo base_url();?>assets/img/digital_farming.jpeg" alt="Card image" style="height: 250px">
-  <div class="card-body">Basic card</div>
+
+<div class="col-sm-1 col-md-1" style="border: 0">
 </div>
 
-<div class="card col-sm-4">
+<div class="card col-sm-4 col-md-3 rounded" style="margin-top: 20px; margin-left: 20">
+  <img class="card-img-top" src="<?php echo base_url();?>assets/img/digital_farming.jpeg" alt="Card image" style="height: 250px">
+  <div class="card-body">Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</div>
+  <a href="#" class="card-link stretched-link">Card link</a>
+</div>
+
+<div class="card col-sm-4 col-md-3 rounded" style="margin-top: 20px; margin-left: 20" >
   <img class="card-img-top" src="<?php echo base_url();?>assets/img/cover.png" alt="Card image" style="height: 250px">
 
-  <div class="card-body">Basic card</div>
-</div>
-<div class="card col-sm-4">
-  <img class="card-img-top" src="<?php echo base_url();?>assets/img/digital_farming.jpeg" alt="Card image" style="height: 250px">
-
-  <div class="card-body">Basic card</div>
+  <div class="card-body">Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</div>
+  <a href="#" class="card-link stretched-link">Card link</a>
 </div>
 
-
-<div class="card col-sm-4">
+<div class="card col-sm-4 col-md-3 rounded" style="margin-top: 20px; margin-left: 20">
   <img class="card-img-top" src="<?php echo base_url();?>assets/img/digital_farming.jpeg" alt="Card image" style="height: 250px">
-  <div class="card-body">Basic card</div>
+
+  <div class="card-body">Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</div>
+  <a href="#" class="card-link stretched-link">Card link</a>
 </div>
 
-<div class="card col-sm-4">
-  <img class="card-img-top" src="<?php echo base_url();?>assets/img/digital_farming.jpeg" alt="Card image" style="height: 250px">
-
-  <div class="card-body">Basic card</div>
 </div>
-<div class="card col-sm-4">
+
+<div class="row">
+<div class="col-sm-1 col-md-1" style="border: 0">
+</div>
+
+<div class="card col-sm-4 col-md-3 rounded" style="margin-top: 20px; margin-left: 20">
+  <img class="card-img-top" src="<?php echo base_url();?>assets/img/digital_farming.jpeg" alt="Card image" style="height: 250px">
+  <div class="card-body">Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</div>
+  <a href="#" class="card-link stretched-link">Card link</a>
+</div>
+
+<div class="card col-sm-4 col-md-3" style="margin-top: 20px; margin-left: 20">
   <img class="card-img-top" src="<?php echo base_url();?>assets/img/digital_farming.jpeg" alt="Card image" style="height: 250px">
 
-  <div class="card-body">Basic card</div>
+  <div class="card-body ">Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</div>
+  <a href="#" class="card-link stretched-link">Card link</a>
+</div>
+
+<div class="card col-sm-4 col-md-3" style="margin-top: 20px; margin-left: 20">
+  <img class="card-img-top" src="<?php echo base_url();?>assets/img/digital_farming.jpeg" alt="Card image" style="height: 250px">
+
+  <div class="card-body">Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</div>
+  <a href="#" class="card-link stretched-link">Card link</a>
 </div>
 
 

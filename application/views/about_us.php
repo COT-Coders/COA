247614af82c3652060ad0f80c4c94a2158004e5d
Maynk <m.charly3015@gmail.com>
2020-04-03 01:05:41 +0530

    home page content

--- application/views/about_us.php
+++ application/views/about_us.php
@@ -1,21 +1,38 @@
 
     
-    <body  style="min-height: 100vh; position: relative;">
+    <body >
         <section class="section">
-            <div class="container">
+            <div class="container-fluid" style="background-color: coral;>
                 <h1 class="title">
-                    About us yap...                
+                    About Campus                
                 </h1>
-                <h1 class="title">
-                    About us yap...                
-                </h1>
-
-                <h1 class="title">
-                    About us yap...                
-                </h1>
-
-
+                <div class="row">
+                    <div class="col-sm-4 col-md-3"> </div>
+                    <div class="col-sm-4 col-md-6">
+                        <p> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                        </p>
+                    </div>
+                </div>
 
             </div>
         </section>
+
+        <section>
+            <div class="container-fluid" style="background-color: cyan; height:200">
+                <h1 class="title">
+                    Our Mission                
+                </h1>
+            </div>
+        </section>
+
+        <section>
+                <div class="container-fluid" style="background-color: silver; height:200">
+                <h1 class="title">
+                    Our Vision                
+                </h1>
+            </div>
+        </section>
+
+
+           
     </body>

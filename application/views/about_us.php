f985da27201552c9ef0b6b11c820192fcf45544c
Maynk <m.charly3015@gmail.com>
2020-04-04 20:41:38 +0530

    about page content

--- application/views/about_us.php
+++ application/views/about_us.php
@@ -2,15 +2,26 @@
     
     <body >
         <section class="section">
-            <div class="container-fluid" style="background-color: coral;>
-                <h1 class="title">
-                    About Campus                
-                </h1>
-                <div class="row">
+            <div class="container-fluid" style="background-color: white; margin-top: 50">
+                
+                <div class="row" style ="margin-left:10; margin-right:10">
                     <div class="col-sm-4 col-md-3"> </div>
                     <div class="col-sm-4 col-md-6">
-                        <p> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                        </p>
+                        <h1 class="title">
+                            About Campus                
+                        </h1>
+                        <div class="row" >
+
+                            <img src="<?php echo base_url();?>assets/img/university.jpg" style="width: 100%">
+
+                            <p> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+
+                            </br>
+                           </br>
+
+                            Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+                            </p>
+                        </div>
                     </div>
                 </div>
 
@@ -18,10 +29,27 @@
         </section>
 
         <section>
-            <div class="container-fluid" style="background-color: cyan; height:200">
-                <h1 class="title">
-                    Our Mission                
-                </h1>
+            <div class="container-fluid" style="background-color: #e2e2e4;">
+                
+                <div class="row" style ="margin-left:10; margin-right:10;">
+                    
+                    <div class="col-sm-4 col-md-3"> </div>
+                    <h1 class="title">
+                        Our Mission                
+                    </h1>
+                    <div class="card-transparent col-sm-4 col-md-3 rounded" style="margin-top: 20px; margin-left: 20; margin-bottom: 100">
+                      <img class="card-img-top" src="<?php echo base_url();?>assets/img/digital_farming.jpeg" alt="Card image" style="height: 250px">
+                      <div class="card-body">Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</div>
+                      
+                    </div>
+
+                    <div class="card-transparent col-sm-4 col-md-3 rounded" style="margin-top: 20px; margin-left: 20; margin-bottom: 100" >
+                      <img class="card-img-top" src="<?php echo base_url();?>assets/img/cover.png" alt="Card image" style="height: 250px">
+
+                      <div class="card-body">Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</div>
+                      
+                    </div>
+                </div>
             </div>
         </section>
 

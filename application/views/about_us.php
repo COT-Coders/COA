02c433d3dc22214b714b477d2c4515fc74d67e11
Maynk <m.charly3015@gmail.com>
2020-04-05 00:38:19 +0530

    about page content

--- application/views/about_us.php
+++ application/views/about_us.php
@@ -7,14 +7,14 @@
                 <div class="row" style ="margin-left:10; margin-right:10">
                     <div class="col-sm-4 col-md-3"> </div>
                     <div class="col-sm-4 col-md-6">
-                        <h1 class="title">
-                            About Campus                
-                        </h1>
+                        <h3>
+                            About               
+                        </h3>
                         <div class="row" >
 
                             <img src="<?php echo base_url();?>assets/img/university.jpg" style="width: 100%">
 
-                            <p> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                            <p class="text-justify" style="margin-top: 10"> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
 
                             </br>
                            </br>
@@ -34,32 +34,166 @@
                 <div class="row" style ="margin-left:10; margin-right:10;">
                     
                     <div class="col-sm-4 col-md-3"> </div>
-                    <h1 class="title">
-                        Our Mission                
-                    </h1>
-                    <div class="card-transparent col-sm-4 col-md-3 rounded" style="margin-top: 20px; margin-left: 20; margin-bottom: 100">
-                      <img class="card-img-top" src="<?php echo base_url();?>assets/img/digital_farming.jpeg" alt="Card image" style="height: 250px">
-                      <div class="card-body">Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</div>
-                      
+                        <div class="col-sm-4 col-md-6">
+                            
+
+                            <div class="row">
+                                <div class="card-transparent col-sm-6 col-md-6 rounded" style="margin-top: 20px;">
+                                  <!--<h3 class="text-center" style="margin-top: 20">
+                                        Our Mission                
+                                  </h3>-->
+                                  <img class="card-img-top" src="<?php echo base_url();?>assets/img/mission.jpg" alt="Card image" style="height: 250px">
+                                  <div class="card-body">
+                                    <h4 style="margin-top: 20">
+                                     Mission               
+                                    </h4>
+                                    <p class="text-justify">
+                                        There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.
+                                    </p>
+                                  </div>
+                                  
+                                </div>
+
+                                <div class="card-transparent col-sm-6 col-md-6 rounded" style="margin-top: 20px;" >
+                                  <!--<h3 class="text-center" style="margin-top: 20">
+                                    Our Vision               
+                                  </h3> -->
+                                  <img class="card-img-top" src="<?php echo base_url();?>assets/img/vision.jpg" alt="Card image" style="height: 250px">
+                                  
+                                  <div class="card-body">
+                                    <h4 style="margin-top: 20">
+                                     Vision               
+                                    </h4>
+                                    <p class="text-justify">
+                                      There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.
+                                    </p>
+                                  </div>
+                                      
+                                </div>
+                            </div>
+                        </div>
                     </div>
+            </div>
+        </section>
 
-                    <div class="card-transparent col-sm-4 col-md-3 rounded" style="margin-top: 20px; margin-left: 20; margin-bottom: 100" >
-                      <img class="card-img-top" src="<?php echo base_url();?>assets/img/cover.png" alt="Card image" style="height: 250px">
 
-                      <div class="card-body">Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</div>
-                      
+        <section>
+            
+
+
+
+        </section>
+             <div class="container-fluid" style="background-color: white;">
+
+                 <div class="row" style ="margin-left:10; margin-right:10;">
+                    <div class="col-sm-4 col-md-3"> </div>
+                    <div class="col-sm-4 col-md-6">
+
+                        <blockquote class="blockquote">
+                          <p class="mb-0 text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+                        </blockquote>
                     </div>
                 </div>
+            </div>
+
+
+
+        <section>
+            <div class="container-fluid" style="background-color: white;">
+                
+                <div class="row" style ="margin-left:10; margin-right:10;">
+                    
+                    <div class="col-sm-4 col-md-3"> </div>
+                        <div class="col-sm-4 col-md-6">
+                            
+
+                            <div class="row">
+                                <div class="card-transparent col-sm-6 col-md-6 rounded" style="margin-top: 20px;">
+                                 
+                                  <img class="card-img-top" src="<?php echo base_url();?>assets/img/faculty.jpg" alt="Card image" style="height: 250px">
+                                  <div class="card-body">
+                                    <h3  style="margin-top: 20">
+                                        Faculty              
+                                      </h3>
+                                      <p class="text-justify">
+                                        A talented and highly respected faculty is the university’s most significant resource. Many are recognized for exceptional scholarship with memberships in such organizations as the American Academy of Arts and Sciences, the National Academy of Sciences, and the National Academy of Engineering.
+                                    </br>
+                                        Our faculty have been awarded Nobel Prizes, Pulitzer Prizes, and the Fields Medal in Mathematics. The success of our faculty is matched by that of our alumni: 11 are Nobel Laureates and another 18 have won Pulitzer Prizes.
+                                    </p>
+                                  </div>
+                                  
+                                </div>
+
+                                <div class="card-transparent col-sm-6 col-md-6 rounded" style="margin-top: 20px;" >
+                                  
+                                  <img class="card-img-top" src="<?php echo base_url();?>assets/img/academic_r1.jpeg" alt="Card image" style="height: 250px">
+
+                                  <div class="card-body">
+                                    <h3  style="margin-top: 20">
+                                        Resources              
+                                    </h3>
+                                    <p class="text-justify">
+                                      Academic resources on campus are among the finest in the world. The University Library is one of the largest public university collections in the world with 13 million volumes in its 37 unit libraries. Annually, 53,000,000 people visit its online catalog. Students have access to thousands of computer terminals in classrooms, residence halls, and campus libraries for use in classroom instruction, study, and research.
+                                  </p>
+                                  </div>
+                                      
+                                </div>
+                            </div>
+                        </div>
+                    </div>
             </div>
         </section>
 
         <section>
-                <div class="container-fluid" style="background-color: silver; height:200">
-                <h1 class="title">
-                    Our Vision                
-                </h1>
+            <div class="container-fluid" style="background-color: white;">
+                
+                <div class="row" style ="margin-left:10; margin-right:10;">
+                    
+                    <div class="col-sm-4 col-md-3"> </div>
+                        <div class="col-sm-4 col-md-6">
+                            
+
+                            <div class="row">
+                                <div class="card-transparent col-sm-6 col-md-6 rounded" style="margin-top: 20px;">
+                                  
+                                  <img class="card-img-top" src="<?php echo base_url();?>assets/img/resercs_sample.jpg" alt="Card image" style="height: 250px">
+                                  <div class="card-body">
+                                    <h3  style="margin-top: 20">
+                                        Research               
+                                    </h3>
+                                  <p class="text-justify">
+                                        At Illinois, our focus on research shapes our identity, permeates our classrooms and fuels our outreach. Fostering discovery and innovation is our fundamental mission. As a public, land-grant university, we have the responsibility to create new knowledge and new ideas and translate these into better ways of working, living and learning for our state, nation and world.
+                                    </br>
+                                        Entrepreneurship flows from the classrooms to Research Park, a space that houses everything from Fortune 500 companies to student-founded startups. We are consistently ranked among the top five universities for NSF-funded research and our total annual research funding exceeds $600 million.
+                                    </p>
+                                  </div>
+                                  
+                                </div>
+
+                                <div class="card-transparent col-sm-6 col-md-6 rounded" style="margin-top: 20px;" >
+                                  
+                                  <img class="card-img-top" src="<?php echo base_url();?>assets/img/students.jpg" alt="Card image" style="height: 250px">
+
+                                  <div class="card-body">
+                                    <h3  style="margin-top: 20">
+                                        Undergraduate Education              
+                                      </h3>
+                                      <p class="text-justify">
+                                      The fundamental promise at Illinois for nearly 150 years has been to provide our undergraduate students with truly transformative educational experiences. Whether these experiences take place in the classroom, in the surrounding community or around the globe, our students leave this campus with the skills, knowledge and the drive to become leaders in their fields and to lead lives of impact in the world. Each year we welcome more than 32,000 undergraduate students across our nine undergraduate divisions – offering nearly 5,000 courses in more than 150 fields of study and awarding about 7,000 new degrees each spring.
+                                  </p>
+                                  </div>
+                                      
+                                </div>
+                            </div>
+                        </div>
+                    </div>
             </div>
         </section>
+
+        
+
+
+
 
 
            

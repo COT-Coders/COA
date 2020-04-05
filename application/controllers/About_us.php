fdfdb46f26f1138efbabf7750693e9f5a57927a2
Samaksh-Narayan-Garg <gargsamaksh1994@gmail.com>
2020-04-05 18:30:53 +0530

    Deleting un-necessary files

--- application/controllers/About_us.php
+++
@@ -1,9 +0,0 @@
-<?php
-	class About_us extends CI_Controller {
-		public function index() {
-			$this->load->view('templets/header');
-			$this->load->view('about_us');
-			$this->load->view('templets/footer');
-		}
-	}
-?>

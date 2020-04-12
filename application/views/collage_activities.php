8a395f1157086fbfb1ecec379e8c951dbaca859c
Maynk <m.charly3015@gmail.com>
2020-04-13 01:10:13 +0530

    activities style update

--- application/views/collage_activities.php
+++ application/views/collage_activities.php
@@ -10,8 +10,8 @@
 				</div> -->
 
 				<div class="card col-sm-4 col-md-3 rounded" style="margin-top: 20px; margin-left: 20;max-height: 500h">
-					  <img class="card-img-top" src="<?php echo base_url();?>assets/img/digital_farming.jpeg" alt="Card image" style="height: 200px">
-					  <div class="card-body text-justify">Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</div>
+					  <img class="card-img-top" src="<?php echo base_url();?>assets/img/digital_farming.jpeg" alt="Card image" style="height: 150px">
+					  <div class="card-body text-justify" style="height: 200px;overflow: hidden;">Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</div>
 					  <a href="#" class="card-link stretched-link"></a>
 					  <div class="card-footer bg-transparent text-muted text-right" style="border: 0;color: white;font-size: 12">
 					    APR 11, 2020 10:15 AM
@@ -19,9 +19,9 @@
 				</div>
 
 					<div class="card col-sm-4 col-md-3 rounded" style="margin-top: 20px; margin-left: 20" >
-					  <img class="card-img-top" src="<?php echo base_url();?>assets/img/cover.png" alt="Card image" style="height: 200px">
+					  <img class="card-img-top" src="<?php echo base_url();?>assets/img/cover.png" alt="Card image" style="height: 150px">
 
-					  <div class="card-body text-justify">Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</div>
+					  <div class="card-body text-justify" style="height: 200px;overflow: hidden;">Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</div>
 					  <a href="#" class="card-link stretched-link"></a>
 					  <div class="card-footer bg-transparent text-muted text-right" style="border: 0;color: white;font-size: 12">
 					    APR 11, 2020 10:15 AM
@@ -29,9 +29,9 @@
 				</div>
 
 				<div class="card col-sm-4 col-md-3 rounded" style="margin-top: 20px; margin-left: 20">
-					  <img class="card-img-top" src="<?php echo base_url();?>assets/img/smartag.jpeg" alt="Card image" style="height: 200px">
+					  <img class="card-img-top" src="<?php echo base_url();?>assets/img/smartag.jpeg" alt="Card image" style="height: 150px">
 
-					  <div class="card-body text-justify">Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</div>
+					  <div class="card-body text-justify" style="height: 200px;overflow: hidden;">Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</div>
 					  <a href="#" class="card-link stretched-link"></a>
 					  <div class="card-footer bg-transparent text-muted text-right" style="border: 0;color: white;font-size: 12">
 					    APR 11, 2020 10:15 AM
@@ -41,8 +41,8 @@
 			
 
 				<div class="card col-sm-4 col-md-3 rounded" style="margin-top: 20px; margin-left: 20;margin-bottom: 20">
-				  <img class="card-img-top" src="<?php echo base_url();?>assets/img/digital_farming.jpeg" alt="Card image" style="height: 200px">
-				  <div class="card-body text-justify">Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</div>
+				  <img class="card-img-top" src="<?php echo base_url();?>assets/img/digital_farming.jpeg" alt="Card image" style="height: 150px">
+				  <div class="card-body text-justify" style="height: 200px;overflow: hidden;">Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</div>
 				  <a href="#" class="card-link stretched-link"></a>
 				  <div class="card-footer bg-transparent text-muted text-right" style="border: 0;color: white;font-size: 12">
 					    APR 11, 2020 10:15 AM
@@ -50,9 +50,9 @@
 				</div>
 
 				<div class="card col-sm-4 col-md-3" style="margin-top: 20px; margin-left: 20;margin-bottom: 20">
-				  <img class="card-img-top" src="<?php echo base_url();?>assets/img/cover.png" alt="Card image" style="height: 200px">
+				  <img class="card-img-top" src="<?php echo base_url();?>assets/img/cover.png" alt="Card image" style="height: 150px">
 
-				  <div class="card-body text-justify">Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</div>
+				  <div class="card-body text-justify" style="height: 200px;overflow: hidden;">Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</div>
 				  <a href="#" class="card-link stretched-link"></a>
 				  <div class="card-footer bg-transparent text-muted text-right" style="border: 0;color: white;font-size: 12">
 					    APR 11, 2020 10:15 AM
@@ -60,9 +60,9 @@
 				</div>
 
 				<div class="card col-sm-4 col-md-3" style="margin-top: 20px; margin-left: 20; margin-bottom: 20" >
-				  <img class="card-img-top" src="<?php echo base_url();?>assets/img/smartag.jpeg" alt="Card image" style="height: 200px">
+				  <img class="card-img-top" src="<?php echo base_url();?>assets/img/smartag.jpeg" alt="Card image" style="height: 150px">
 
-				  <div class="card-body text-justify">Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</div>
+				  <div class="card-body text-justify" style="height: 200px;overflow: hidden;">Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</div>
 				  <a href="#" class="card-link stretched-link"></a>
 				  <div class="card-footer bg-transparent text-muted text-right" style="border: 0;color: white;font-size: 12">
 					    APR 11, 2020 10:15 AM
@@ -70,19 +70,35 @@
 				</div>
 
 
-				<div class="card col-sm-4 col-md-3" style="margin-top: 20px; margin-left: 20; margin-bottom: 20" >
-				  <img class="card-img-top" src="<?php echo base_url();?>assets/img/smartag.jpeg" alt="Card image" style="height: 200px">
-
-				  <div class="card-body text-justify">
-				  		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus imperdiet, nulla et dictum interdum, nisi lorem egestas vitae scel<span id="dots">...</span><span id="more">erisque enim ligula venenatis dolor. Maecenas nisl est, ultrices nec congue eget, auctor vitae massa. Fusce luctus vestibulum augue ut aliquet. Nunc sagittis dictum nisi, sed ullamcorper ipsum dignissim ac. In at libero sed nunc venenatis imperdiet sed ornare turpis. Donec vitae dui eget tellus gravida venenatis. Integer fringilla congue eros non fermentum. Sed dapibus pulvinar nibh tempor porta.</span></p>
-
-					<button onclick="myFunction()" id="myBtn">Read more</button>
-				  </div>
+				<div class="card col-sm-4 col-md-3 rounded" style="margin-top: 20px; margin-left: 20;margin-bottom: 20">
+				  <img class="card-img-top" src="<?php echo base_url();?>assets/img/digital_farming.jpeg" alt="Card image" style="height: 150px">
+				  <div class="card-body text-justify" style="height: 200px;overflow: hidden;">Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</div>
 				  <a href="#" class="card-link stretched-link"></a>
 				  <div class="card-footer bg-transparent text-muted text-right" style="border: 0;color: white;font-size: 12">
 					    APR 11, 2020 10:15 AM
 					  </div>
-				</div>				
+				</div>
+
+				<div class="card col-sm-4 col-md-3" style="margin-top: 20px; margin-left: 20;margin-bottom: 20">
+				  <img class="card-img-top" src="<?php echo base_url();?>assets/img/cover.png" alt="Card image" style="height: 150px">
+
+				  <div class="card-body text-justify " style="height: 200px;overflow: hidden;">Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</div>
+				  <a href="#" class="card-link stretched-link"></a>
+				  <div class="card-footer bg-transparent text-muted text-right" style="border: 0;color: white;font-size: 12">
+					    APR 11, 2020 10:15 AM
+					  </div>
+				</div>
+
+				<div class="card col-sm-4 col-md-3" style="margin-top: 20px; margin-left: 20; margin-bottom: 20" >
+				  <img class="card-img-top" src="<?php echo base_url();?>assets/img/smartag.jpeg" alt="Card image" style="height: 150px">
+
+				  <div class="card-body text-justify" style="height: 200px;overflow: hidden;">Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco. Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</div>
+				  <a href="#" class="card-link stretched-link"></a>
+				  <div class="card-footer bg-transparent text-muted text-right" style="border: 0;color: white;font-size: 12">
+					    APR 11, 2020 10:15 AM
+					  </div>
+				</div>
+				
 
 			</div>
 		</div>
--- application/views/templets/header_common.php
+++ application/views/templets/header_common.php
@@ -11,9 +11,16 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+
+  <!--<style>
+    #more {display: none;}
+  </style> -->
+
+
   
   <link rel="stylesheet" href="<?php echo base_url();?>assets/css/custom_nav.css">
   <link rel="stylesheet" href="<?php echo base_url();?>assets/css/footer.css">
+
   
 
 	<!------ Include the above in your HEAD tag ---------->

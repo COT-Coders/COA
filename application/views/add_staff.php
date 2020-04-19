801e92b0b99a11792c38579eaccf00149c712f43
Samaksh-Narayan-Garg <gargsamaksh1994@gmail.com>
2020-04-19 19:49:49 +0530

    Updating staff pages for insertion/display of address and phone no.

--- application/views/add_staff.php
+++ application/views/add_staff.php
@@ -8,9 +8,11 @@
 		<div class="fl_right">
 		<?php echo validation_errors('<div class="error"><i class="fa fa-exclamation-triangle"></i>', '</div>'); ?>
 		
-		<input type="text" id="staff_name" name="staff_name" placeholder="Staff Name" required>
-		<input type="text" id="staff_desig" name="staff_desig" placeholder="Designation" required>
-		<input type="text" id="staff_info" name="staff_info" placeholder="Personal Info" required>
+		<input type="text" id="staff_name" name="staff_name" placeholder="Staff Name" value="<?php echo set_value('staff_name'); ?>" required />
+		<input type="text" id="staff_desig" name="staff_desig" placeholder="Designation" value="<?php echo set_value('staff_desig'); ?>" required/>
+		<input type="text" id="staff_info" name="staff_info" placeholder="Personal Info" value="<?php echo set_value('staff_info'); ?>" required/>
+		<input type="text" id="staff_address" name="staff_address" placeholder="Address" value="<?php echo set_value('staff_address'); ?>" required/>
+		<input type="text" id="staff_ph_no" name="staff_ph_no" placeholder="Phone Number" value="<?php echo set_value('staff_ph_no'); ?>" required/>
 		<!--<input type="image" src="<?php echo base_url();?>assets/layout/images/sign_in.gif" id="signin" alt="Sign In" />-->
 
 		<button type="submit" class="btn btn-primary">Add</button>

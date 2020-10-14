9cce35efd27c7dbb5bb482fe884a7c83541503f6
Maynk <m.charly3015@gmail.com>
2020-10-14 23:11:16 +0530

    scrolling issue removed from front page

--- application/views/templates/header_home.php
+++ application/views/templates/header_home.php
@@ -8,7 +8,7 @@
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-	<!--<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>-->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
   
 	<link rel="stylesheet" href="<?php echo base_url();?>assets/css/custom_nav.css">
 	<link rel="stylesheet" href="<?php echo base_url();?>assets/css/footer_new.css">
